02d7671bd562eea45cce56dc45d32de31aec2bb2
matheusldeperes <matheusldeperes@gmail.com>
2026-01-19 17:02:23 -0300

    aosifhadds

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from googleapiclient.http import MediaIoBaseUpload
 # --- CONFIGURAÇÕES ---
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 # Substitua pelo ID da pasta Satte Alam no seu Google Drive
-PARENT_FOLDER_ID = '1nv3SIx2cWzOJnCa6iS4aBDdBklWNvsVj' 
+PARENT_FOLDER_ID = '1p-GmmzGCeLM0EX2xYba5ufK3--goB4iN' 
 
 # Inicialização do Estado para as Fotos
 if 'lista_fotos' not in st.session_state:
